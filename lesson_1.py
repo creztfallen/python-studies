decbdcb08f398e1c9aa2e9bf0dcf955ddcb1010b
Mateus <creztfallen@M.S.M>
2022-10-05 12:13:11 -0300

    variables, types, io, operators

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+print("FILL THE FORM BELOW")
+name = input('Name:')
+cpf = input('CPF:')
+address = input('Address:')
+height = input('Height:')
+tel = input('Tel:')
+
+print('\n\n\n\n')
+
+print('User Name:', name)
+print('User CPF:', cpf)
+print('User Address:', address)
+print('User Height:', height)
+print('User Telphone:', tel)
