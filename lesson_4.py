9d8196e44020a914807f29c50e62aa08dc4010ba
Mateus Moreno <61795256+creztfallen@users.noreply.github.com>
2022-11-18 14:51:55 -0300

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -1,26 +1,26 @@
 names = ['Geralt', 'Dijkstra', 'Jarpen']
 name = 'Mateus'
-# int_list = range(0, 100, 2)
+int_list = range(0, 100, 2)
 
-# for name in names:
-#     print(name)
+for name in names:
+    print(name)
     
 
-# for i in int_list:
-#     print(i)    
+for i in int_list:
+    print(i)    
 
-# for i in range(len(names)):
-#     print(names[i])
+for i in range(len(names)):
+    print(names[i])
     
-# for char in name:
-#     print(char)    
+for char in name:
+    print(char)    
 
-# i= 1
-# while i < 10:
-#     print('i is still less than 10: ', i)
-#     i += 1
+i= 1
+while i < 10:
+    print('i is still less than 10: ', i)
+    i += 1
     
-# print('\nEnd of while: ', i)    
+print('\nEnd of while: ', i)    
 
 guests = int(input('How many people were invited to the party: '))
 
